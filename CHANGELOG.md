8277baf4d38179caad81907a95944a3af83b205d
Bernie <bernie.wangbj@gmail.com>
2014-07-03 16:47:01 +0800

    implement part of function resolvePromise

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
 ####2014-7-2
 + 增加了 resolvePromise 方法对应 promise/a+ 的 promise resolution procedure,但是没有实现。
 + 修正了当 fulfilledHandler 和 rejectedHandler 不是 function 的行为，按照的是 promise/a+ 的spec。
+
+####2014-7-3
++ 部分实现了 resolvePromise 方法，但是其中如果 x 为 object 或者 function 时如何处理则还没有看懂promise/a+ 的 spec。
--- bpromise.js
+++ bpromise.js
@@ -8,22 +8,75 @@
         this.nextPromise = new BPromise();
     }
 
+    // @const
     BPromise.STATUS_PENDING = 'pending';
+    // @const
     BPromise.STATUS_FULFILLED = 'fulfilled';
+    // @const
     BPromise.STATUS_REJECTED = 'rejected';
 
+    // @static
     BPromise.isFunction = function ( f ) {
         // 这么判断是否满足所有浏览器，还需要调研
         return typeof f === 'function';
-    }
-
+    };
+    // @static
+    BPromise.isObject = function ( o ) {
+        return Object.prototype.toString.call( o ) === '[object Object]';
+    };
+    // @static
     BPromise.isValidStatus = function ( s ) {
         return [ BPromise.STATUS_PENDING, BPromise.STATUS_FULFILLED, BPromise.STATUS_REJECTED ].indexOf( s ) > -1;
-    }
+    };
+    // @static
+    BPromise.isPromise = function ( obj ) {
+        return obj.then && obj.getStatus;
+    };
+
+    BPromise.prototype.getFulfilledValue = function () {
+        return this.ffv;
+    };
+
+    BPromise.prototype.getRejectedReason = function () {
+        return this.rjr;
+    };
 
     // promise resolution procdure
     BPromise.prototype.resolvePromise = function ( promise, value ) {
-        // TODO
+
+        if ( promise === value ) throw new TypeError( 'Parameters of resolvePromise can not be the same object.' );
+
+        if ( BPromise.isPromise( value ) ) {
+
+            var valueStatus = value.getStatus();
+            switch( valueStatus ) {
+
+                case BPromise.STATUS_PENDING:
+                    value.then( function ( v ) {
+                        promise.resolve( v );
+                    }, function ( r ) {
+                        promise.reject( r );
+                    } )
+                    break;
+
+                case BPromise.STATUS_FULFILLED:
+                    promise.resolve( value.getFulfilledValue() );
+                    break;
+
+                case BPromise.STATUS_REJECTED:
+                    promise.reject( value.getRejectedReason() );
+                    break;
+            }
+        } else if ( BPromise.isObject( value ) || BPromise.isFunction( value ) ) {
+            // TODO
+        } else {
+            promise.resolve( value );
+        }
+
+    };
+
+    BPromise.prototype.getStatus = function () {
+        return this.status;
     };
 
     BPromise.prototype.resolve = function ( value, promise ) {
